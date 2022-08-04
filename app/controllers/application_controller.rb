94d2c583b95c0cee420bee2ac1f54af7eede3032
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-08-03 22:22:50 -0500

    Feat(controllers/ - views/): [Create controllers, views, and helpers to the splash screen]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
-  before_action :authenticate_user!
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
@@ -11,5 +10,17 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :email, :password, :password_confirmation, :current_password)
     end
+  end
+
+  def after_sign_in_path_for(_resource)
+    groups_path
+  end
+
+  private
+
+  def remember_page
+    session[:previous_pages] ||= []
+    session[:previous_pages] << url_for(params.to_unsafe_h) if request.get?
+    session[:previous_pages] = session[:previous_pages].uniq.first(2)
   end
 end
