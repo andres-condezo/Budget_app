5df24e9e9a5b51c2123d61baab866abaecaf91d9
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-08-06 12:36:12 -0500

    Test(spec/requests/): [Add groups and budgets request tests]

--- spec/requests/budgets_spec.rb
+++ spec/requests/budgets_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Budgets', type: :request do
+  include Devise::Test::IntegrationHelpers
+
+  before(:each) do
+    @user = User.new(name: 'user1', email: 'user1@mail.com', password: '123456')
+    @user.save
+
+    @group = Group.new(name: 'group1',
+                       icon: Rack::Test::UploadedFile.new('spec/book_icon.png', 'image/png'),
+                       author_id: @user.id)
+    @group.save
+
+    @budget = Budget.new(name: 'budget1',
+                         amount: 2,
+                         author_id: @user.id,
+                         group_id: @group.id)
+    @budget.save
+  end
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      sign_in user
+      get category_path(@group.id)
+    end
   end
 end
--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Groups', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  include Devise::Test::IntegrationHelpers
+
+  before(:each) do
+    @user = User.new(name: 'user1', email: 'user1@mail.com', password: '123456')
+    @user.save
+
+    @group = Group.new(name: 'group1',
+                       icon: Rack::Test::UploadedFile.new('spec/book_icon.png', 'image/png'),
+                       author_id: @user.id)
+    @group.save
+  end
+
+  describe 'GET#index' do
+    before do
+      sign_in @user
+      get groups_path
+    end
+
+    it 'response to html' do
+      expect(response.content_type).to include 'text/html'
+    end
+  end
+
+  describe 'GET#new' do
+    before do
+      sign_in @user
+      get groups_path
+      get new_group_path
+    end
+
+    it 'responds to html' do
+      expect(response.content_type).to include 'text/html'
+    end
   end
 end
