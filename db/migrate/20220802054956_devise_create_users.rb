7bcd51848a666fd0aee742e2847fe89410b13284
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-08-06 15:18:20 -0500

    Delete commented code

--- db/migrate/20220802054956_devise_create_users.rb
+++ db/migrate/20220802054956_devise_create_users.rb
@@ -11,22 +11,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
       ## Rememberable
       t.datetime :remember_created_at
-
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
-
-
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
   end
 
   def self.down
